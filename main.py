85ae265ddc5dd4a3213cb7cd79c7dc3d446b3fec
Lance <act762@msn.com>
2022-10-08 23:30:37 +0800

    2022-10-08_2

--- main.py
+++ main.py
@@ -126,22 +126,19 @@ def on_error(context, code, info):
     # stop()
 
 
-
-
-
 if __name__ == '__main__':
     '''
-        strategy_id策略ID, 由系统生成
-        filename文件名, 请与本文件名保持一致
-        mode运行模式, 实时模式:MODE_LIVE回测模式:MODE_BACKTEST
-        token绑定计算机的ID, 可在系统设置-密钥管理中生成
-        backtest_start_time回测开始时间
-        backtest_end_time回测结束时间
-        backtest_adjust股票复权方式, 不复权:ADJUST_NONE前复权:ADJUST_PREV后复权:ADJUST_POST
-        backtest_initial_cash回测初始资金
-        backtest_commission_ratio回测佣金比例
-        backtest_slippage_ratio回测滑点比例
-        '''
+    strategy_id策略ID, 由系统生成
+    filename文件名, 请与本文件名保持一致
+    mode运行模式, 实时模式:MODE_LIVE回测模式:MODE_BACKTEST
+    token绑定计算机的ID, 可在系统设置-密钥管理中生成
+    backtest_start_time回测开始时间
+    backtest_end_time回测结束时间
+    backtest_adjust股票复权方式, 不复权:ADJUST_NONE前复权:ADJUST_PREV后复权:ADJUST_POST
+    backtest_initial_cash回测初始资金
+    backtest_commission_ratio回测佣金比例
+    backtest_slippage_ratio回测滑点比例
+    '''
     run(strategy_id='83402db0-3f32-11ed-bcc8-4ae7da2ac5af',
         filename='main.py',
         mode=MODE_LIVE,
